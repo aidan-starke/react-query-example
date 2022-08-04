7d51e7a65000bba39ca51639b910d66f620f0756
aidan-starke <starke.aidan@gmail.com>
2022-08-05 11:43:07 +1200

    Update favicon & page title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,6 +8,14 @@ function MyApp({ Component, pageProps }: AppProps) {
 	return (
 		<QueryClientProvider client={queryClient}>
 			<Hydrate state={pageProps.dehydratedState}>
+				<h1 className="font-mono text-center text-2xl pt-6">
+					<a
+						href="/"
+						className="p-2 border-blue-600 hover:border-b-2 duration-100"
+					>
+						Acala Explorer
+					</a>
+				</h1>
 				<Component {...pageProps} />
 			</Hydrate>
 		</QueryClientProvider>
