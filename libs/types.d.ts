f3b4c7434ce8fa56bed2305894cf6eb53ab99e2f
aidan-starke <starke.aidan@gmail.com>
2022-08-09 15:15:52 +1200

    Set up `zustand`

--- libs/types.d.ts
+++ libs/types.d.ts
@@ -2,6 +2,8 @@ export interface AcalaBlock {
 	__typename?: "Block";
 	id: string;
 	number?: any | null;
+	timestamp?: any | null;
+	parentHash?: string | null;
 	extrinsics: {
 		__typename?: "ExtrinsicsConnection";
 		edges: Array<{
@@ -10,3 +12,16 @@ export interface AcalaBlock {
 		}>;
 	};
 }
+
+export interface AcalaTransfer {
+	__typename?: "Transfer";
+	id: string;
+	fromId?: string | null;
+	toId?: string | null;
+	tokenId?: string | null;
+	amount?: string | null;
+	timestamp?: any | null;
+}
+
+export type AcalaBlocks = Array<AcalaBlock | null>;
+export type AcalaTransfers = Array<AcalaTransfer | null>;
