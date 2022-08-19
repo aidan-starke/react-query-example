27dd3b5d9e9bec454c4b423b284c0ee3b9afe576
aidan-starke <starke.aidan@gmail.com>
2022-08-19 12:00:46 +1200

    Check for `window`

--- libs/stores/theme.ts
+++ libs/stores/theme.ts
@@ -3,13 +3,16 @@ import { atom } from "jotai";
 type ThemeState = "Light" | "Dark";
 
 const themeAtom = atom<ThemeState>(
-	(localStorage.getItem("colour-theme") as ThemeState) ?? "Light"
+	(typeof window !== "undefined"
+		? (localStorage.getItem("colour-theme") as ThemeState)
+		: "Light") ?? "Light"
 );
 
 export const themeStateAtom = atom(
 	(get) => get(themeAtom),
 	(_, set, newTheme: ThemeState) => {
 		set(themeAtom, newTheme);
-		localStorage.setItem("colour-theme", newTheme);
+		if (typeof window !== "undefined")
+			localStorage.setItem("colour-theme", newTheme);
 	}
 );
